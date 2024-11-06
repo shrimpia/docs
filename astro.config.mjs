10a0d051d58709449dc43f1470d0a98532f728ad
Ebise <lutica@shrimpia.network>
2024-11-06 21:30:09 +0900

    リンク切れを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightLinksValidator from 'starlight-links-validator';
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,6 +22,9 @@ export default defineConfig({
 		starlight({
 			title: '帝国ドキュメント（仮設）',
 			favicon: '/favicon.png',
+			plugins: [
+				starlightLinksValidator(),
+			],
 			logo: {
 				src: '@/assets/logo.png',
 				alt: 'シュリンピア',
